9e5694872184e005a7893c8538a6b82506e12b4b
Ashar <ashar.a.hmd11@gmail.com>
2025-01-30 06:39:51 +0500

    initialcommit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const URI = 'mongodb+srv://metatech674:helloworld22@cluster0.qb31q.mongodb.net/'
-
+mongoose.set("strictQuery", false);
 const connectToMongo = () => mongoose.connect(URI, () => {
     console.log("Connected to Mongo Successfully")
 })
